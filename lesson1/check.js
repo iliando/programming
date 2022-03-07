0c03d5a49d0e9ca2f3d710c5ba031f38145b06f6
test <test1>
2022-03-07 15:14:10 +0600

    tex

--- lesson1/check.js
+++ lesson1/check.js
@@ -1,20 +1,3 @@
-// const height1 = prompt("Введите первое число!")
-// const height2 = prompt("Введите второе число!")
-//
-// if (height1 < height2)
-// {
-//     alert(`${height1} Меньше ${height2}`);
-// }
-// else if (height1 > height2)
-// {
-//     alert(`${height1} Больше ${height2}`);
-// }
-// else
-// {
-//     alert(`${height1} Равны ${height2}`);
-// }
-//
-
 const length1 = prompt("Введите длину первого массива: ");
 const length2= prompt("Введите длину второго массив: ");
 
